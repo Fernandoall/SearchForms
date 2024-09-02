525a61de1a2f329b09a302edc63b806671ea9827
Fernando Gomes <fernando.macena06@gmail.com>
2024-09-02 10:16:38 -0300

    Aplicando conceito de props no React

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Article } from "./components/article/Article";
 import { Navbar } from "./components/Navbar/Navbar";
 
+import "./App.css";
+
 class App extends React.Component {
   render() {
     return (
@@ -9,10 +11,45 @@ class App extends React.Component {
         <Navbar />
 
         <section id="articles">
-          <Article title="Exemplo" />
-          <Article />
-          <Article />
-          <Article />
+          <Article
+            title="Designing Dashboards."
+            provider="Nasa"
+            description="If you use this site regularly and would like to help keep the site
+            on the Internet, please consider donating a small sum to help pay
+            for the hosting and bandwidth bill. There is no minimum donation,
+            any sum is appreciated - click here to donate using PayPal. Thank
+            you for your support. Donate bitcoin: 16UQLq1HZ3CNwhvgrarV6pMoA2CDj"
+          />
+
+          <Article
+            title="Vibrant Portraits of 2020"
+            provider="SpaceNews"
+            description="If you use this site regularly and would like to help keep the site
+            on the Internet, please consider donating a small sum to help pay
+            for the hosting and bandwidth bill. There is no minimum donation,
+            any sum is appreciated - click here to donate using PayPal. Thank
+            you for your support. Donate bitcoin: 16UQLq1HZ3CNwhvgrarV6"
+          />
+
+          <Article
+            title="36 days of Malayalam type"
+            provider="SpaceNews"
+            description="If you use this site regularly and would like to help keep the site
+            on the Internet, please consider donating a small sum to help pay
+            for the hosting and bandwidth bill. There is no minimum donation,
+            any sum is appreciated - click here to donate using PayPal. Thank
+            you for your support. Donate bitcoin: 16UQLq1HZ3CNwhvgrarV6"
+          />
+
+          <Article
+            title="Designing Dashboards."
+            provider="Nasa"
+            description="If you use this site regularly and would like to help keep the site
+            on the Internet, please consider donating a small sum to help pay
+            for the hosting and bandwidth bill. There is no minimum donation,
+            any sum is appreciated - click here to donate using PayPal. Thank
+            you for your support. Donate bitcoin: 16UQLq1HZ3CNwhvgrarV6pMoA2CDj"
+          />
         </section>
       </>
     );
--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -11,15 +11,9 @@ export class Article extends React.Component {
 
         <div className="article-infos">
           <h2>{this.props.title}</h2>
-          <h3>NASA</h3>
+          <h3>{this.props.provider}</h3>
 
-          <p>
-            If you use this site regularly and would like to help keep the site
-            on the Internet, please consider donating a small sum to help pay
-            for the hosting and bandwidth bill. There is no minimum donation,
-            any sum is appreciated - click here to donate using PayPal. Thank
-            you for your support. Donate bitcoin: 16UQLq1HZ3CNwhvgrarV6pMoA2CDj
-          </p>
+          <p>{this.props.description}</p>
         </div>
       </article>
     );
